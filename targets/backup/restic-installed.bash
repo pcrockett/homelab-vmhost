20771a027f1d8b327eb2d3334068627a94978318
Phil Crockett <contact@philcrockett.com>
2026-01-02 15:11:47 +0100

    install restic

--- targets/backup/restic-installed.bash
+++ targets/backup/restic-installed.bash
@@ -1,12 +1,10 @@
 #!/usr/bin/env blarg
 
 # shellcheck disable=SC2034  # PACKAGE is used in `rush-package-installed` snippet
-PACKAGE="backup-cli"
-
-depends_on restic-installed
+PACKAGE="restic"
 
 satisfied_if() {
-  command -v backup
+  command -v "${PACKAGE}"
 }
 
 snippet rush-package-installed
