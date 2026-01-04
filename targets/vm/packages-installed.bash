8f3998fc8fa027dce14c7892c649a53a60154155
Phil Crockett <contact@philcrockett.com>
2026-01-04 09:39:12 +0100

    correct qemu-kvm package name

--- targets/vm/packages-installed.bash
+++ targets/vm/packages-installed.bash
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034  # variables appear unused, but are used inside snippet
 
 PACKAGES=(
-  qemu-kvm
+  qemu-system-x86
   libvirt-daemon-system
   bridge-utils
   dnsmasq-base
