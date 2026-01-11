085e895499b2d8d444d52f09f583af956d97aee9
Phil Crockett <contact@philcrockett.com>
2026-01-11 20:23:16 +0100

    get disk encryption password from bitwarden

--- targets/boot/tpm-unlock-configured.bash
+++ targets/boot/tpm-unlock-configured.bash
@@ -1,16 +1,22 @@
 #!/usr/bin/env blarg
 
-depends_on clevis-installed
+depends_on clevis-installed core/with-umask-installed bitwarden/cli-installed
 
 satisfied_if() {
   checkpoint_is_current
 }
 
 apply() {
+  satisfy bitwarden/synced
+  load_bw_item machine.lab01
+
+  local disk_encryption_password
+  disk_encryption_password="$(bw_value disk.encryption.password)"
+
   lsblk --filter 'TYPE == "crypt"' --output PKNAME --noheadings \
     | sort --unique \
     | while IFS= read -r device_name; do
-      echo "${DISK_ENCRYPTION_PASSWORD}" | as_root clevis luks bind -k - -d "/dev/${device_name}" tpm2 '{}'
+      echo "${disk_encryption_password}" | as_root clevis luks bind -k - -d "/dev/${device_name}" tpm2 '{}'
     done
 
   as_root update-initramfs -u -k all
