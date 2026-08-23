5df1ddac06a4e1cc182e3b8965c6d079ba514d5c
Phil Crockett <contact@philcrockett.com>
2026-08-23 15:12:16 +0200

    settings for vms

--- targets/vm/scratch-vm-created.bash
+++ targets/vm/scratch-vm-created.bash
@@ -1,14 +1,25 @@
 #!/usr/bin/env blarg
+#
+# provision a disposable VM
+#
 
 depends_on incus-initialized
 
-# provision a disposable VM
 VM_NAME=scratch
+CPU_COUNT=2
+MEMORY=2GiB
+MEMORY_ENFORCE=soft
+ROOT_SIZE=30GiB
 
 satisfied_if() {
   incus info "${VM_NAME}"
 }
 
 apply() {
-  incus create images:archlinux "${VM_NAME}" --vm
+  incus create images:archlinux "${VM_NAME}" \
+    --vm \
+    --config limits.cpu="${CPU_COUNT}" \
+    --config limits.memory="${MEMORY}" \
+    --config limits.memory.enforce="${MEMORY_ENFORCE}" \
+    --device root,size="${ROOT_SIZE}"
 }
