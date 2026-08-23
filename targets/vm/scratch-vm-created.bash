2f0bef8c4f949e0c2f6bdfec12222dcc4c977c4d
Phil Crockett <contact@philcrockett.com>
2026-08-23 21:46:47 +0200

    wip: one step closer to getting incus VMs working

--- targets/vm/scratch-vm-created.bash
+++ targets/vm/scratch-vm-created.bash
@@ -20,5 +20,29 @@ apply() {
     --config limits.cpu="${CPU_COUNT}" \
     --config limits.memory="${MEMORY}" \
     --config security.secureboot=false \
-    --device root,size="${ROOT_DISK_SIZE}"
+    --device root,size="${ROOT_DISK_SIZE}" \
+    --device root,io.bus=nvme
+
+  # - `io.bus=virtio-scsi` is the default. it crashes something important in our
+  #   virtualization stack.
+  # - `io.bus=virtio-blk` seems to _delay_ the crash, but it certainly doesn't fix it.
+  # - `io.bus=nvme` seems to at least survive the coolify install process, but the
+  #   upgrade at the end times out waiting for containers to start.
+  #
+  # UNFORTUNATELY because of this io bus change, `incus-growpart.service` inside the
+  # VM crashes because it's looking for `/dev/sda2`, but under this driver it's called
+  # `/dev/nvme0n1p2`. so TODO here: patch that service file to get this working. it
+  # should end up like:
+  #
+  #     [Unit]
+  #     Description=Incus - grow root partition
+
+  #     [Service]
+  #     Type=oneshot
+  #     ExecStartPre=-/usr/sbin/growpart /dev/nvme0n1 2
+  #     ExecStart=/usr/sbin/resize2fs /dev/nvme0n1p2
+
+  #     [Install]
+  #     WantedBy=default.target
+  #
 }
