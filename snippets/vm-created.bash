2175d87a3695ba0621ba7b578e0433a5c9a80d15
Phil Crockett <contact@philcrockett.com>
2026-09-06 07:17:00 +0200

    default vm disk size 10 -> 15 GiB

--- snippets/vm-created.bash
+++ snippets/vm-created.bash
@@ -5,7 +5,7 @@ depends_on vm/incus-initialized
 VM_NAME="${VM_NAME:?must specify vm name at least}"
 CPU_COUNT="${CPU_COUNT:-2}"
 MEMORY="${MEMORY:-2GiB}"
-ROOT_DISK_SIZE="${ROOT_DISK_SIZE:-10GiB}"
+ROOT_DISK_SIZE="${ROOT_DISK_SIZE:-15GiB}"
 INCUS_IMAGE="${INCUS_IMAGE:-images:archlinux}"
 
 satisfied_if() {
