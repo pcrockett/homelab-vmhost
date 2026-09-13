9deea21e3c0fb1456810763480135897dc1288b5
Phil Crockett <contact@philcrockett.com>
2026-09-13 19:05:19 +0200

    test config contents

--- targets/cli/bat-configured.bash
+++ targets/cli/bat-configured.bash
@@ -4,7 +4,7 @@ REPO_PATH="${BLARG_MODULE_DIR}/config/bat"
 SYSTEM_PATH=~/.config/bat
 
 satisfied_if() {
-  files_are_same "${REPO_PATH}" "${SYSTEM_PATH}"
+  files_are_same "${REPO_PATH}/config" "${SYSTEM_PATH}/config"
 }
 
 apply() {
