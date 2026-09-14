663a53c31e973658a4fb40efa7ab37ebde58a4dc
Phil Crockett <contact@philcrockett.com>
2026-09-14 12:00:01 +0200

    fix bat config

--- targets/cli/bat-configured.bash
+++ targets/cli/bat-configured.bash
@@ -1,13 +1,14 @@
 #!/usr/bin/env blarg
 
-REPO_PATH="${BLARG_MODULE_DIR}/config/bat"
-SYSTEM_PATH=~/.config/bat
+REPO_PATH="${BLARG_MODULE_DIR}/config/bat/config"
+SYSTEM_PATH=~/.config/bat/config
 
 satisfied_if() {
-  files_are_same "${REPO_PATH}/config" "${SYSTEM_PATH}/config"
+  files_are_same "${REPO_PATH}" "${SYSTEM_PATH}"
 }
 
 apply() {
   rm -rf "${SYSTEM_PATH}"
-  install --directory "${REPO_PATH}" "${SYSTEM_PATH}"
+  mkdir -p "$(dirname "${SYSTEM_PATH}")"
+  install --mode u=rw,g=r,o=r "${REPO_PATH}" "${SYSTEM_PATH}"
 }
